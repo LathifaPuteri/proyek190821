a0deaf9b4c79a3a3f9f6aca96197e3d907fcf5f7
LathifaPuteri <73085162+LathifaPuteri@users.noreply.github.com>
2021-08-26 14:23:56 +0700

    Add files via upload

---
+++ hasiltugas2.php
@@ -0,0 +1,22 @@
+<?php
+
+$Nama = $_POST['Nama'];
+$Jenis_Kelamin = $_POST['Jenis_Kelamin'];
+$Agama = $_POST['Agama'];
+$Alamat = $_POST['Alamat'];
+$TTL = $_POST['TTL'];
+$Hobi = $_POST['Hobi'];
+$Cita_Cita = $_POST['Cita-Cita'];
+$Usia = $_POST['Usia'];
+$Asal_Sekolah = $_POST['Asal_Sekolah'];
+
+echo "Nama : ".$Nama."<br/>";
+echo "Jenis Kelamin : ".$Jenis_Kelamin."<br/>";
+echo "Agama : ".$Agama."<br/>";
+echo "Alamat : ".$Alamat."<br/>";
+echo "TTL : ".$TTL."<br/>";
+echo "Hobi : ".$Hobi."<br/>";
+echo "Cita-Cita : ".$Cita_Cita."<br/>";
+echo "Usia : ".$Usia."<br/>";
+echo "Asal Sekolah : ".$Asal_Sekolah."<br/>";
+?>
---
+++ tugas2.php
@@ -0,0 +1,32 @@
+<form method = "post" action = "hasiltugas2.php">
+    <label> Masukkan Nama </label><br/>
+    <input type = "text" name = "Nama"><br/>
+
+    <label> Pilih Jenis Kelamin </label><br/>
+    <input type="radio" name="Jenis_Kelamin" value="Pria">Pria
+    <input type="radio" name="Jenis_Kelamin" value="Wanita">Wanita
+    <br/>
+
+    <label> Masukkan Agama </label><br/>
+    <input type = "text" name = "Agama"><br/>
+
+    <label> Masukkan Alamat </label><br/>
+    <input type = "text" name = "Alamat"><br/>
+
+    <label> Masukkan TTL </label><br/>
+    <input type = "text" name = "TTL"><br/>
+
+    <label> Masukkan Hobi </label><br/>
+    <input type = "text" name = "Hobi"><br/>
+
+    <label> Masukkan Cita-Cita </label><br/>
+    <input type = "text" name = "Cita-Cita"><br/>
+
+    <label> Masukkan Usia </label><br/>
+    <input type = "text" name = "Usia"><br/>
+
+    <label> Masukkan Asal Sekolah </label><br/>
+    <input type = "text" name = "Asal_Sekolah"><br/>
+
+    <input type="submit" value="oke">
+</form>
